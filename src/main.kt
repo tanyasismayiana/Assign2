d17de327df68ce54b1261d13135f7f480f886a94
tanyamayiana <tanyamayiana@gmail.com>
2022-02-20 23:17:42 +0300

    Add my file

---
+++ src/main.kt
@@ -0,0 +1,21 @@
+fun main(){
+    Hello()
+    modulus(13, 4)
+    sum(32, 45, 22,14)
+    funfact()
+}
+fun Hello(){
+    val name = "Tanya"
+    println("Hello "+name)
+}
+fun modulus(a:Int, b:Int){
+    var modulus = a%b
+    println(modulus)
+}
+fun sum(a:Int, b:Int, c:Int, d:Int){
+    var sum = a+b+c+d
+    println(sum)
+}
+fun funfact(){
+    println("i love watching")
+}
